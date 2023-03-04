3fcb7cc2c6bf3c217543cab9476abc1dc0a5b603
Cristi_ROG <cristian.givan.it@gmail.com>
2023-03-04 23:12:13 +0200

    commit 12

--- src/main/java/com/example/testcicd/controller/ProductController.java
+++ src/main/java/com/example/testcicd/controller/ProductController.java
@@ -15,7 +15,7 @@ public class ProductController {
 
     @GetMapping("/test")
     public String test(){
-        String test = "test2";
+        String test = "test3";
         return test;
     }
 
